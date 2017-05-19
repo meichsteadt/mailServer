6c393df0079ebead5db6fe80ce07b14924fb2400
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-05-19 16:05:01 -0700

    change cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4200', 'http://ichorstudios.com', 'http://beta.ichorstudios.com'
+    origins '*'
 
     resource '*',
       headers: :any,
