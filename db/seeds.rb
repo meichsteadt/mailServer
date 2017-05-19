995fd3b9370b61fd9bb23bef7ad61f6f530737b3
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-05-19 16:47:47 -0700

    add user to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(key: "LkVqrMnS3??2@&MuM[L%,hsnE-@a8G;R[&[Th2[Tcd+Ap}tXgf)qp~Z/`#GLgVW<gXSvv.[4A.eF79]^$pcFQ/zj*c~)-Tr]/<>^[FpDXq!a27r6cwX}6nQN$7@grR", password: "QGC9@z%6xwkA&)zb9]`yqbwjNW[Z!n9>8(3.[TCQ[S8~6=U2!).]8y>U)D:n3[}fXzrvyr}9h5d@5b@GC(gfHwE}+kG?xZ<#(fwBh;4B%Wh%g[59ZZ>?fX=hQc@3M^")
