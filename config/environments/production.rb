7e364e426fbc3466a56861782d5b938ba33da7dc
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-05-19 16:27:01 -0700

    move serve static assets config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
 
-config.serve_static_assets = false
+  config.serve_static_assets = false
+end
